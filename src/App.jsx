4feb90c934a0f50a13998521fa69ff4df0ea3d74
unknown <deepanshusharma551@gmail.com>
2022-11-26 18:09:11 +0530

    forgotpage added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Home from "./pages/Home";
 import {AuthProvider} from "./Auth"
 import PrivateRoute from "./PrivateRoute"
 import Login from "./pages/Login/Login";
-
+import Register from "./pages/Register/Register";
+import Forgot from "./pages/Forgot/Forgot";
 
 const App=()=> {
 
@@ -17,11 +18,14 @@ const App=()=> {
         <Switch>
 
            
-            {/* <PrivateRoute exact path="/hero" component={Hero}></PrivateRoute> */}
+            <PrivateRoute exact path="/" component={Home}></PrivateRoute>
             
-            <Route exact path="/" component={Home}></Route>
+            
            
             <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/forgot" component={Forgot}></Route>
+            <Route exact path="/**" component={Error}></Route>
         </Switch>
         </Router>
            
